512edd32e076394ecdb2e91b6e2fa1755a63cbc4
harishkumar6381 <emvharish20@gmail>
2022-02-03 11:41:33 +0530

    Folder structure

---
+++ src/org/cts/test/login/CourseDetailes.java
@@ -0,0 +1,24 @@
+package org.cts.test.login;
+
+public class CourseDetailes {
+	
+	private void javaCourse() {
+		
+		System.out.println("java class is started");
+		
+	}
+	
+	private void pythoneCourse() {
+		
+		System.out.println("pythone course is started");
+		
+	}
+
+	
+	public static void main(String[] args) {
+		CourseDetailes c = new CourseDetailes();
+		c.javaCourse();
+		c.pythoneCourse();
+				
+	}
+}
